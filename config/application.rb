8cb6637764851eb5f784934a2f230dca929aa8fb
panda <a17877yun@gmail.com>
2014-04-11 18:04:51 +0800

    test

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,7 @@ module TestMongo
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
 
-    config.assets.enabled = true
+    #config.assets.enabled = true
 
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
