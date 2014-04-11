becd0f5dedcf26a6ae8f6ae382ca0b6f9afde198
panda <a17877yun@gmail.com>
2014-04-11 18:11:06 +0800

    test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,8 +55,8 @@ TestMongo::Application.configure do
   # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  config.action_controller.asset_host = "http://curio.streams.tw/"
-  config.action_mailer.asset_host = "http://curio.streams.tw/"
+  config.action_controller.asset_host = "http://curio.streams.tw"
+  config.action_mailer.asset_host = "http://curio.streams.tw"
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
