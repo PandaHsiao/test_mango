7356552e6dc46393d042e293e08aaece1db6249a
panda <a17877yun@gmail.com>
2014-11-11 22:31:49 +0800

    test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,7 +19,7 @@ TestMongo::Application.configure do
   # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
   # config.action_dispatch.rack_cache = true
 
-  config.assets.initialize_on_precompile = false
+  config.assets.initialize_on_precompile = true # orgin is false
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
   config.serve_static_assets = true
@@ -34,9 +34,9 @@ TestMongo::Application.configure do
 
   config.assets.compile = true
   #config.assets.precompile << /(^[^_\/]|\/[^_])[^\/]*$/
-  #config.assets.precompile =  ['*.js', '*.scss','*.css','*.js.coffee']
-  config.assets.precompile =  ['application.js', 'home.js','jquery.pagination.js','yoolyooly.js' ,'application.css.scss',
-                               'home.css.scss', 'pagination.css', 'yollyooly.css.scss']
+  config.assets.precompile =  ['*.js', '*.scss','*.css','*.js.coffee']
+  #config.assets.precompile =  ['application.js', 'home.js','jquery.pagination.js','yoolyooly.js' ,'application.css.scss',
+   #                            'home.css.scss', 'pagination.css', 'yollyooly.css.scss']
 
 
 
