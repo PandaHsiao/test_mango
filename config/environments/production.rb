bd11fdbce8c4c5bb02004fd5339a2825d774124b
panda <a17877yun@gmail.com>
2014-11-12 11:53:14 +0800

    dgf

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -34,7 +34,7 @@ TestMongo::Application.configure do
 
   config.assets.compile = true
   config.assets.precompile << /(^[^_\/]|\/[^_])[^\/]*$/
-  config.assets.precompile =  ['*.js', '*.scss','*.css','*.js.coffee']
+  config.assets.precompile +=  ['variables.scss', '*.js', '*.scss','*.css','*.js.coffee']
 
   #config.assets.precompile += %w( *.css *.js *.scss *.js.coffee )
   #config.assets.precompile =  ['application.js', 'home.js','jquery.pagination.js','yoolyooly.js' ,'application.css.scss',
