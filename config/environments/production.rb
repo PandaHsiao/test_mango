d7434c9376693edf29281207e149b136a15dbce5
panda <a17877yun@gmail.com>
2014-11-12 17:10:17 +0800

    sdf

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,7 +36,7 @@ TestMongo::Application.configure do
   #config.assets.precompile << /(^[^_\/]|\/[^_])[^\/]*$/
 
   #config.assets.precompile += %w( bootstrap.custom.scss )
-  #config.assets.precompile += %w( application.js yoolyooly.js application.css home.css yoolyooly.css )
+  config.assets.precompile += %w( application.js yoolyooly.js application.css home.css yoolyooly.css )
 
   #config.assets.precompile =  ['*.js', '*.scss','*.css','*.js.coffee']
 
